81c36d380fca38d9abf8e3d23c69f8431d69fae9
KantoCanto <bruno.canto.neves@gmail.com>
2023-11-12 18:22:50 +0000

    dynamic routing added

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import EventListView from "../views/EventListView.vue";
+import EventsDetailsView from "../views/EventsDetailsView.vue";
 import AboutView from "../views/AboutView.vue";
 
 const router = createRouter({
@@ -9,6 +10,13 @@ const router = createRouter({
       path: "/",
       name: "event-list",
       component: EventListView,
+    },
+
+    {
+      path: "/events/:id",
+      name: "event-details",
+      props: true,
+      component: EventsDetailsView,
     },
     {
       path: "/about",
