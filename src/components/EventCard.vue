43a1ed8392039fee110ee921cf248f052f52c074
KantoCanto <bruno.canto.neves@gmail.com>
2023-11-12 12:16:26 +0000

    starting point for the vue_mastery course.
    First SFC -> HomeView.vue

---
+++ src/components/EventCard.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps({
+  event: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="event-card">
+    <h2>{{ event.title }}</h2>
+    <span>@{{ event.time }} on {{ event.date }}</span>
+  </div>
+</template>
+
+<style scoped>
+.event-card {
+  padding: 1.5rem;
+  width: 20rem;
+  cursor: pointer;
+  border: 0.01rem solid #39495c;
+  margin-bottom: 1.3rem;
+}
+
+.event-card:hover {
+  transform: scale(1.02);
+  box-shadow: 0 0.1rem 1rem 0 rgba(0, 0, 0, 0.2);
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,60 @@
+<script setup>
+import EventCard from "../components/EventCard.vue";
+import { ref } from "vue";
+
+const events = ref([
+  {
+    id: 123455,
+    category: "animal welfare",
+    title: "Cat Adoption Day",
+    description: "Find your new feline friend at this event.",
+    location: "Meow Town",
+    date: "January 28, 2022",
+    time: "12:00",
+    petsAllowed: true,
+    organizer: "Kat Lydi",
+  },
+  {
+    id: 234566,
+    category: "animal welfare",
+    title: "Dog Adoption Day",
+    description: "Find your new doggo friend at this event.",
+    location: "BarkVille",
+    date: "January 29, 2022",
+    time: "12:00",
+    petsAllowed: true,
+    organizer: "Dough Borky",
+  },
+  {
+    id: 123123,
+    category: "animal welfare",
+    title: "Food for Animals",
+    description: "Help feed starving animals at this event.",
+    location: "Zoo City",
+    date: "January 30, 2022",
+    time: "12:00",
+    petsAllowed: true,
+    organizer: "???",
+  },
+]);
+</script>
+
+<template>
+  <h1>Events For Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+}
+
+h1 {
+  text-align: center;
+}
+</style>
